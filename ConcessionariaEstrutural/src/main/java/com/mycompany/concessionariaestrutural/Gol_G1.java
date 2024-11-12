73807db59f777836925f4f9270bbf11e9da6da1c
Gabriel Alcantara Alves dos Santos <141934868+GabrielAlcantara01@users.noreply.github.com>
2024-11-11 21:21:08 -0300

    Método Estrutural

---
+++ ConcessionariaEstrutural/src/main/java/com/mycompany/concessionariaestrutural/Gol_G1.java
@@ -0,0 +1,15 @@
+package com.mycompany.concessionariaestrutural;
+
+/**
+ *
+ * @author Aluno-Lab
+ */
+public class Gol_G1 implements Carro{
+    public double getCost() {
+        return 20000;
+    }
+
+    public String getDescription() {
+        return "Gol Quadrado";
+    }
+}
